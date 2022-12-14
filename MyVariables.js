143404936c8fc5143859a4d1caacf3e9a110a46b
Christine <exp16240@adobe.com>
2022-12-13 16:27:24 -0800

    updated training files
    updates

--- MyVariables.js
+++ MyVariables.js
@@ -1,7 +1,7 @@
 var s_account = "adobecx1czak";
 var s = s_gi(s_account);
 
-s.trackingServer = "training.data.adobedc.net";
+s.trackingServer = "czak.data.adobedc.net";
 
 s.pageName = "Czak home page";
 s.eVar1 = "Example value";
